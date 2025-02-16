ec21bd3da80d0794f0963a2f5403e5f1e837894e
Joseph Wardell <jay@jaywardell.me>
2025-02-16 09:53:49 -0500

    NSPasteboard.url works the same whether the string was entered as a string or as a data

--- Tests/PasteboardURLExtractorTests/NSPasteboard+URLTests.swift
+++ Tests/PasteboardURLExtractorTests/NSPasteboard+URLTests.swift
@@ -33,6 +33,39 @@ struct NSPasteBoard_URL {
         #expect(expected == sut.url)
     }
     
+    @Test("If there's an URL in the pasteboard as type .string, set as Data, then return that")
+    func url_in_pasteboard_for_type_STRING_as_data() async throws {
+        let sut = makeSUT()
+        
+        let expected = URL(string:"http://hi")!
+        let data = expected.absoluteString.data(using: .utf8)
+        sut.setData(data, forType: .string)
+        
+        #expect(expected == sut.url)
+    }
+
+    @Test("If there's an URL in the pasteboard as type .URL, set as Data, then return that")
+    func url_in_pasteboard_for_type_URL_as_data() async throws {
+        let sut = makeSUT()
+        
+        let expected = URL(string:"http://hi")!
+        let data = expected.absoluteString.data(using: .utf8)
+        sut.setData(data, forType: .URL)
+        
+        #expect(expected == sut.url)
+    }
+
+    @Test("If there's an URL in the pasteboard as type .URL, set as Data, then return that")
+    func url_in_pasteboard_for_type_URL_as_data2() async throws {
+        let sut = makeSUT()
+        
+        let expected = URL(string:"http://hi")!
+        let data = expected.dataRepresentation
+        sut.setData(data, forType: .URL)
+        
+        #expect(expected == sut.url)
+    }
+
     // MARK: - Administrative
     
     @Test("Ensure that SUT works as expected") func isNilIfPasteboardIsEmpty() async throws {
